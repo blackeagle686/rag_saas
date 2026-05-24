90a2b9081ea872d4ca8b5a796fdca4053d752bfa
blackeagle686 <mathematecs1@gmail.com>
2026-05-24 09:30:33 +0300

    Format filename column definition in Document model

--- db/models/document.py
+++ db/models/document.py
@@ -38,8 +38,10 @@ class Document(TimestampMixin, Base):
         nullable=False,
         index=True,
     )
-    
-    filename: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    filename: Mapped[str] = mapped_column(
+        String(255),
+         nullable=False)
     file_type: Mapped[FileType] = mapped_column(String(10), nullable=False)
     status: Mapped[DocumentStatus] = mapped_column(
         String(20),
