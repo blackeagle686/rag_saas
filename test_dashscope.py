51a2c2aae3a22e52269e4f198adc56e79ca23644
blackeagle686 <mathematecs1@gmail.com>
2026-06-06 13:23:55 +0300

    Update API key in test_dashscope.py

--- test_dashscope.py
+++ test_dashscope.py
@@ -1,6 +1,6 @@
 import dashscope
 
-dashscope.api_key = "sk-ws-H.HLXLMP.bsb6.MEUCIQDGgGDCO-IC4PgfH3-M_zdsFm3vWz7z9sL9Eb99c4keQIgDgWfWhyQwFh3gYngwdr_P82ItOMgnm_Mz38U2_jgJdQ"
+dashscope.api_key = "sk-ws-H.HLXLMP.bsb6.MEUCIQDGgGDCO-IC4PgfH3-M_zdsFm3vWz7z9 sL9Eb99c4keQIgDgWfWhyQwFh3gYngwdr_P82ItOMgnm_Mz38U2_jgJdQ"
 
 resp = dashscope.TextEmbedding.call(
     model="text-embedding-v4",
