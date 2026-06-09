72c314153c131c34216a8870c4b83614a99eea6d
blackeagle686 <mathematecs1@gmail.com>
2026-06-09 12:12:58 +0300

    Add ragaas/urls.py with API endpoints

---
+++ ragaas/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import (
+    RegisterView, TenantSettingsView, NamespaceViewSet, NamespaceDocumentListView,
+    ApiKeyViewSet, IngestView, QueryView
+)
+
+router = DefaultRouter()
+router.register(r'namespaces', NamespaceViewSet, basename='namespace')
+router.register(r'keys', ApiKeyViewSet, basename='apikey')
+
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    
+    path('tenant/settings/', TenantSettingsView.as_view(), name='tenant-settings'),
+    
+    path('namespaces/<str:name>/docs/', NamespaceDocumentListView.as_view(), name='namespace-docs'),
+    
+    path('ingest/', IngestView.as_view(), name='ingest'),
+    path('query/', QueryView.as_view(), name='query'),
+    
+    path('', include(router.urls)),
+]
