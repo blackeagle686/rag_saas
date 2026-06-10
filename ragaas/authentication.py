3b008d33909271e1b983299dfbfc39f812066d10
blackeagle686 <mathematecs1@gmail.com>
2026-06-10 18:31:34 +0300

    Add authentication import to rest_framework

--- ragaas/authentication.py
+++ ragaas/authentication.py
@@ -1,4 +1,4 @@
-from rest_framework import exceptions
+from rest_framework import authentication, exceptions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class JWTCookieAuthentication(JWTAuthentication):
