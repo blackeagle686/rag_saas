52566ad6e8359d400738b8e1bb2fac574988d314
blackeagle686 <mathematecs1@gmail.com>
2026-06-10 18:37:06 +0300

    Fix Tenant import path in billing_views.py

--- ragaas/api/billing_views.py
+++ ragaas/api/billing_views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from rest_framework import views, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .models import Tenant
+from ragaas.models import Tenant
 import uuid
 import logging
 
