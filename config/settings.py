a80d701c89a608f108fdbf2e5736f5ad8baa1972
blackeagle686 <mathematecs1@gmail.com>
2026-06-10 19:08:30 +0300

    Add localhost:5173 to CORS allowed origins

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5000',
     'http://127.0.0.1:5000',
     'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
