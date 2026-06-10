86319ab7755ebc87e40a58117af31574b9356535
blackeagle686 <mathematecs1@gmail.com>
2026-06-10 18:41:15 +0300

    Update CORS settings and add whitelist

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173', 'http://127.0.0.1:5173']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+]
+CORS_ORIGIN_WHITELIST = CORS_ALLOWED_ORIGINS
 CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
