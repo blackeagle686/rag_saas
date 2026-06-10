e19757f2453984bd9990f8b53f126c20f7a8aaef
blackeagle686 <mathematecs1@gmail.com>
2026-06-10 18:42:44 +0300

    Add CORS allowed methods and headers settings

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,27 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ORIGIN_WHITELIST = CORS_ALLOWED_ORIGINS
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'ragaas.security.authentication.JWTCookieAuthentication',
