ce2b9ab6c65cd1eb6f3c8c9b8781c4040ded5483
blackeagle686 <mathematecs1@gmail.com>
2026-06-24 23:28:47 +0300

    Add CORS regex for localhost origins

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
 ]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http://localhost:\d+$",
+    r"^http://127\.0\.0\.1:\d+$",
+]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
